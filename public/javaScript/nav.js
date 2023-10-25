747f272b8e934f3e08f9311d17bd55c2bacff2c1
ImmerAlex <105159979+ImmerAlex@users.noreply.github.com>
2023-10-25 21:00:57 +0200

    Add files via upload

--- public/javaScript/nav.js
+++ public/javaScript/nav.js
@@ -2,6 +2,12 @@ const burger = document.querySelector(".burger");
 const leftNavContainer = document.querySelector(".leftNavContainer");
 
 burger.addEventListener("click", () => {
-    burger.classList.toggle("active")
-    leftNavContainer.classList.toggle("active")
-})
+    burger.classList.toggle("active");
+    leftNavContainer.classList.toggle("active");
+
+    if (leftNavContainer.classList.contains("active")) {
+        document.body.style.overflow = "hidden";
+    } else {
+        document.body.style.overflow = "auto";
+    }
+});
---
+++ public/javaScript/shop.js
@@ -0,0 +1,70 @@
+import { dataListe } from "./data.js";
+const data = dataListe();
+
+const containerCard = document.querySelector(".container-card");
+const buttonShowMore = document.querySelector(".containerItem button");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    let nbrCard = 0;
+    for (let i = 0; i < data.length; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("item-card");
+        if (nbrCard > 8) {
+            newDiv.classList.add("hide");
+        };
+        newDiv.setAttribute("data-type", data[i].type);
+        newDiv.setAttribute("data-price", data[i].price);
+
+        newDiv.innerHTML = ` 
+        <a href="product_page.html">
+            <img src="${data[i].image}" alt="">
+        </a>
+        <div>
+            <h1>${data[i].image.split("/").pop().split(".")[0].split("_").join(" ")}</h1>
+            <p class="price">${data[i].price}</p>
+        </div>
+        `;
+
+        nbrCard++;
+        containerCard.appendChild(newDiv);
+    }
+
+    const showMore = () => {
+        const hideItems = document.querySelectorAll(".item-card.hide");
+        for (let i = 0; i < 9; i++) {
+            if (hideItems[i]) {
+                hideItems[i].classList.remove("hide");
+            }
+        }
+    }
+
+    buttonShowMore.addEventListener("click", showMore);
+})
+
+
+const filterCheckbox = document.querySelectorAll(".item-filter.type div input");
+
+filterCheckbox.forEach(checkBox => {
+    checkBox.addEventListener("click", () => {
+        filterCheckbox.forEach(falseCheck => {
+            falseCheck.checked = false;
+        });
+        checkBox.checked = true;
+        showFiltred(checkBox.getAttribute("data-type"));
+    });
+});
+
+const showFiltred = (type) => {
+    const allItemCard = document.querySelectorAll(".item-card", ".item-card.hide");
+
+    allItemCard.forEach(item => {
+        item.classList.add("hide")
+        if (type === "tout") {
+            item.classList.remove("hide")
+        }
+        else if (item.getAttribute("data-type") === type) {
+            item.classList.remove("hide")
+        };
+    });
+};
